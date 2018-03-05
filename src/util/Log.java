9d9b0e50320c72cdd454ee764d7110c43c86a773
arvebratt <a.arvebratt@gmail.com>
2018-03-05 14:13:44 +0100

    Update Log.java
    new toString method for printing Audit log.

--- src/util/Log.java
+++ src/util/Log.java
@@ -1,6 +1,19 @@
 package util;
 
 public class Log {
+	private String person;
+	private Operation operation;
+	private String time;
+	private boolean status;
+	
 	public Log(String person, Operation operation, String time, boolean status) {
+		this.person = person;
+		this.operation = operation;
+		this.time = time;
+		this.status = status;
 	}
-}
+	
+	public String toString() {
+		return "Person: " + person + "\nOperation: " + operation.toString() + 
+				"\nTime: " + time + "\nStatus: " + status;
+	}
